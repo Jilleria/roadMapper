fdf0c6bba867ec46cdf9d18864bff7181dad3133
Wenjun Shu <wjshu@thoughtworks.com>
2017-03-23 11:30:09 +0800

    first demo

---
+++ src/canvas.vue
@@ -0,0 +1,29 @@
+<template>
+    <body>
+        <div>
+            <canvas width="300" height="150" style="border:1px solid #BBB;" v-insert-message="exampleContent"></canvas>
+            <input type="text" v-model="exampleContent" />
+            <span>{{ exampleContent }}</span>
+        </div>
+    </body>
+</template>
+
+<script>
+  let model = { exampleContent: 'vue' }
+  export default {
+    directives: {
+            replace: false,
+            insertMessage: function() {
+                var canvasElement = this.el;
+                var ctx = canvasElement.getContext("2d");
+                ctx.clearRect(0, 0, 300, 150);
+                ctx.fillStyle = "black";
+                ctx.font = "20px Georgia";
+                ctx.fillText(model.exampleContent, 10, 50);
+            }
+        },
+  	data() {
+      return model;
+    },
+  }
+</script>
